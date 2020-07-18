2c67a7165d4e32f1f4f24b582c975d439df855a5
Rony Moura <ronymmoura@gmail.com>
2020-07-18 07:19:04 -0300

    feat(new feature): creating client

---
+++ client/src/components/Greetings/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import { Container, Image, Text } from './styles'
+
+const Greetings: React.FC = () => {
+  return (
+    <Container>
+      <Image
+        src="https://www.vectorlogo.zone/logos/reactjs/reactjs-icon.svg"
+        alt="ReactJS logo"
+      />
+      <Text>Hello there!</Text>
+    </Container>
+  )
+}
+
+export default Greetings
